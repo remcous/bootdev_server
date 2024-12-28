2a6bf525277d9492d90f2390aaeccec39f058617
remcous <67499364+remcous@users.noreply.github.com>
2024-12-28 15:09:45 -0800

    Chapter 4 completed

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -23,37 +23,15 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerHits)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidate)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	server := http.Server{
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
+	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
 
-	server.ListenAndServe()
-}
-
-func (cfg *apiConfig) handlerHits(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`
-		<html>
-			<body>
-				<h1>
-					Welcome, Chirpy Admin
-				</h1>
-				<p>
-					Chirpy has been visited %d times!
-				</p>
-			</body>
-		</html>`,
-		cfg.fileserverHits.Load())),
-	)
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, req)
-	})
+	log.Printf("Serving files from %s on post: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
